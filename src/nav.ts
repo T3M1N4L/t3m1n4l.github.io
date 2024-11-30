20a3288fdc9a4a1e04a1e9dc737e83aaca8b1aab
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-11-30 00:22:07 +0000

    :sob:

--- src/nav.ts
+++ src/nav.ts
@@ -1,5 +1,5 @@
 export default [
-    { title: 'home', link: './', emoji: ':house:' },
+    { title: 'home', link: '/', emoji: ':house:' },
     { title: 'blog', link: '/blog.html', emoji: ':memo:' },
     { title: 'socials / contact', link: '/socials.html', emoji: ':globe_with_meridians:' },
     { title: 'guestbook', link: 'https://t3rm1n4l.atabook.org', emoji: ':open_book:' },
